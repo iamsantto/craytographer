12f5711718faa19cdb8e77008a385b110b9722d1
Santhosh <santhosh@keepworks.com>
2021-05-20 15:26:49 +0530

    added hire photographer form

---
+++ _assets/javascripts/hireForm.js
@@ -0,0 +1,25 @@
+// Toggle Hire Form
+function showHireForm() {
+  var formBox = document.getElementById("form-box");
+  var formEl = document.getElementById("hire-form");
+
+  var tripetto = TripettoServices.init({ token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiQktqeEluQ0xvYzh0VVdMQzRPQlNQNlBqOE5SaEVNVHlSYzdtd3NQVVp5TT0iLCJkZWZpbml0aW9uIjoiSGwvVEdVQVExYTNhRkRpQTFXanF3ODlyRTRnRHc4MVk2SEp5YXBOSlAyYz0iLCJ0eXBlIjoiY29sbGVjdCJ9.f0qdVyLRluLxDtMkgwyohh_JpmOMSQKwnUGmm7SOz9g" });
+
+  TripettoAutoscroll.run({
+      element: formEl,
+      definition: tripetto.definition,
+      styles: tripetto.styles,
+      l10n: tripetto.l10n,
+      locale: tripetto.locale,
+      translations: tripetto.translations,
+      attachments: tripetto.attachments,
+      onSubmit: tripetto.onSubmit
+  });
+
+  formBox.style.display = "block"
+}
+
+function hideHireForm() {
+  var formBox = document.getElementById("form-box");
+  formBox.style.display = "none"
+}
